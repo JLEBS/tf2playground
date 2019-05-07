b6823bbf9d16d86149253fdc053aaffe096bfa32
Jacques Le Breuilly <jacques.lebreuilly@sidigital.co>
2019-05-07 10:19:50 +0100

    testing second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. First Commit!
+          Edit <code>src/App.js</code> and save to reload. Second sodhfsiofhs!
         </p>
         <a
           className="App-link"

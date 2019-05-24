b85fd10fec21bf80bbba0f2bf48d58018fb81e62
Jacques Le Breuilly <jacques.lebreuilly@sidigital.co>
2019-05-24 12:06:44 +0100

    Another Pie Chart

--- my-app/src/components/profile/infoRectangle.js
+++ my-app/src/components/profile/infoRectangle.js
@@ -611,6 +611,7 @@ const RADIAN = Math.PI / 180;
 
 //recharts-layer recharts-pie-labels contains text
 
+
 class SimplePieChart extends React.Component {
 	render () {
   	return (
@@ -621,9 +622,11 @@ class SimplePieChart extends React.Component {
             label
             outerRadius={100} 
             fill="#009eff"
+            legendType={'circle'}
+            
             >
         	{
-          	data.map((entry, index, color) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
           }
         </Pie>
         <Tooltip/>
@@ -634,6 +637,7 @@ class SimplePieChart extends React.Component {
 
 const COLORSs = ['#009eff', 'gold', 'green', 'purple', '#ff7200', '#EB008A'];
 
+//OLD PIE CHARTS
 const pieOptions = {
     title: "",
     pieHole: 0.6,
@@ -666,13 +670,8 @@ const pieOptions = {
       left: 0,
       top: 0,
       width: "100%",
-      height: "80%"
-    },
-    animation: {
-        duration: 1000,
-         easing: 'in',
-       startup: true
-}
+      height: "100%"
+    }
    
   };
   class NewChart extends React.Component {
@@ -696,12 +695,87 @@ const pieOptions = {
     }
   }
 
+
+  //CURRENT VERSION
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+      fill, payload, percent, value } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+  
+    return (
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#009EFF">{`${value} Wins`}</text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="red">
+          {`(Rate ${(percent * 100).toFixed(2)}%)`}
+        </text>
+      </g>
+    );
+  };
+    
+class TwoLevelPieChart extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {activeIndex: 0}
+    }
+  
+    onPieEnter = (data, index) => {
+      this.setState({
+        activeIndex: index,
+      });
+    }
+
+      render () {
+        return (
+          <PieChart className='modify' width={400} height={300} >
+          <Pie 
+              activeIndex={this.state.activeIndex}
+            activeShape={renderActiveShape} 
+            data={data} 
+            cx={200} 
+            cy={120} 
+            innerRadius={60}
+            outerRadius={80} 
+            fill={COLORS[3]}
+            onMouseEnter={this.onPieEnter}
+
+    
+          />
+         </PieChart>
+      );
+    }
+  }
+
 const PercentageContainer = ({allWinStats}) => {
-    // allWinStats.sort(function (a) {
-    //    console.log(a.winData);
-    // });
-    // //console.log(classStats);
-    // console.log(allWinStats.sort(allWinStats.winData));
 
     return (
         <>
@@ -714,8 +788,9 @@ const PercentageContainer = ({allWinStats}) => {
                     </YetAgainAnotherFlex>
                 </ClassWrapper>
              ))}
-            <NewChart/>
-                <SimplePieChart/>
+                {/* <NewChart/>
+                <SimplePieChart/> */}
+                <TwoLevelPieChart/>
           
         </>
     );
